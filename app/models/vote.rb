356a50edd4397b6248915546e4aee8d2b000b353
junaidq <xcoder27@gmail.com>
2018-08-13 13:56:50 +0500

    Create Method in Model About - Total Size of Votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,11 +2,16 @@ class Vote < ApplicationRecord
   belongs_to :comment
   belongs_to :user
 
-  def self.thumbs_up_total
-    self.where(vote_status: true).size
+  def self.thumbs_up_total(comment)
+    comment.votes.where(vote_status: true).size
   end
 
-  def self.thumbs_down_total
-    self.where(vote_status: false).size
-  end 
+  def self.thumbs_down_total(comment)
+    comment.votes.where(vote_status: false).size
+  end
+
+  def self.vote_up_down_total(comment)
+    Vote.thumbs_up_total(comment) - Vote.thumbs_down_total(comment)
+  end
+
 end
